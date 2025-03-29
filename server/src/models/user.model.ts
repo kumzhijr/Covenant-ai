331705e8cb978cff7414f90101f6deaf03853161
kumzhijr <ponsar.kumzhi@gmail.com>
2025-03-30 00:19:25 +0400

    Add new dependencies and update user model; restructure global styles

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,2 +1,17 @@
-// This file is no longer needed as Prisma replaces Mongoose for database interactions.
-// You can safely delete this file.
+import { model, Schema } from "mongoose";
+
+
+export interface IUser {
+    googleId: string;
+    email: string;
+    displayName: string;
+    profilePicture: string;
+}
+
+const UserSchema:Schema = new Schema({
+    googleId: { type: String, required: true, unique: true},
+    email: { type: String, required: true, unique: true },
+    displayName: { type: String, required: true,},
+    profilePicture: { type: String, required: true,},
+});
+export default model<IUser>("User", UserSchema);
