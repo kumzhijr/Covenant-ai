0b003ef95590d2bca3c46979014889d6d216481d
kumzhijr <ponsar.kumzhi@gmail.com>
2025-04-03 07:09:26 +0400

    Implement authentication middleware and error handling; update header component and add current user route

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -11,13 +11,8 @@ const navItems: { name: string; href: string }[] = [
     { name: "Privacy Policy", href: "/privacy" },
 ];
 
-function googleSignIn() :Promise<void> {
+function googleSignIn(): Promise<void> {
     return new Promise((resolve) => {
-        // const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
-        // const redirectUri = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI;
-        // const scope = "profile email";
-        // const responseType = "token";
-        // const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
         window.location.href = "http://localhost:8080/auth/google";
         resolve();
     });
@@ -26,14 +21,14 @@ function googleSignIn() :Promise<void> {
 export function Header() {
     const pathname = usePathname();
 
-    const user = true;
+    // const user = true;
 
     return (
         <header className="sticky px-4 top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
             <div className="container flex h-16 items-center">
                 <div className="mr-4 hidden md:flex">
                     <Link href={"/"} className="mr-6 flex items-center space-x-2">
-                        Covenantai(LOGO)
+                        Covenantai
                     </Link>
                     <nav className="flex items-center space-x-7 text-sm font-medium">
                         {navItems.map((item) => (
@@ -44,9 +39,9 @@ export function Header() {
                     </nav>
                 </div>
                 <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-                <div className="w-full flex-1 md:w-auto md:flex-none space-x-2 hidden md:flex">
-                    <Button onClick={googleSignIn}>Get Started</Button>
-                </div>    
+                    <div className="w-full flex-1 md:w-auto md:flex-none space-x-2 hidden md:flex">
+                        <Button onClick={googleSignIn}>Get Started</Button>
+                    </div>
                 </div>
             </div>
         </header>
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -16,5 +16,13 @@ router.get(
     }
 );
 
+router.get("/current-user", (req, res) => {
+    if (req.isAuthenticated()) {
+      res.json(req.user);
+    } else {
+      res.status(401).json({ error: "Unauthorized" });
+    }
+  });
+
 
 export default router;
